25703eb08d0e4af171e6f7d229530f1216452fb9
kirstynrowen <50176404+kirstynrowen@users.noreply.github.com>
2023-06-08 14:20:28 -0700

    if statements for rendering license links, testing render license section function

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -22,13 +22,33 @@ function renderLicenseBadge(license) {
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-
+  if (license === 'MIT') {
+    return '[MIT](https://opensource.org/licenses/MIT)';
+  }
+  if (license === 'Apache 2.0') {
+    return '[Apache 2.0](https://opensource.org/licenses/Apache-2.0)';
+  }
+  if (license === 'BSD 3') {
+    return '[BSD 3-Clause](https://opensource.org/licenses/BSD-3-Clause)';
+  }
+  if (license === 'GPL 3.0') {
+    return '[GPL 3.0](https://www.gnu.org/licenses/gpl-3.0)';
+  }
+  if (license === 'Unlicense') {
+    return '[The Unlicense](http://unlicense.org/)';
+  }
+  return '';
 }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-
+  if (license === 'None') {
+    return '';
+  }
+  return `##License
+  
+  This application is licensed under the ${renderLicenseLink(license)} license.`;
 }
 
 // TODO: Create a function to generate markdown for README
