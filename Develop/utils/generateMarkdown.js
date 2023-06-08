9f3f9b09feec261ad4b9710aa695cdd03c8023e6
kirstynrowen <50176404+kirstynrowen@users.noreply.github.com>
2023-06-08 14:48:54 -0700

    cleaning up code, removed unneccessary comments

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,4 +1,4 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
+//Returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
   if (license === 'MIT') {
@@ -19,7 +19,7 @@ function renderLicenseBadge(license) {
   return '';
 }
 
-// TODO: Create a function that returns the license link
+// Returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
   if (license === 'MIT') {
@@ -51,7 +51,7 @@ function renderLicenseSection(license) {
   This application is licensed under the ${renderLicenseLink(license)} license.`;
 }
 
-// TODO: Create a function to generate markdown for README
+// Generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
